5294fa467e4e703b994a0f8bb18bbf8c62ff19ca
Howard Choi <hchoi84@gmail.com>
2020-06-18 10:00:43 -0700

    Minor touchups

--- NewAgeUI/ViewModels/ResendEmailConfirmationLinkViewModel.cs
+++ NewAgeUI/ViewModels/ResendEmailConfirmationLinkViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace NewAgeUI.ViewModels
 {
   public class ResendEmailConfirmationLinkViewModel
   {
+    [Display(Name = "Email Address")]
+    [DataType(DataType.EmailAddress)]
     public string EmailAddress { get; set; }
   }
 }
