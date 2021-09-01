5d55ad51fab661270e280d96639647c4f386acb5
hchoi84 <hchoi84@gmail.com>
2021-09-01 16:35:49 -0700

    Created background service

---
+++ NewAgeUI/BackgroundServices/BackgroundTaskQueue.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace NewAgeUI.BackgroundServices
+{
+  public interface IBackgroundTaskQueue
+  {
+    ValueTask Enqueue(Func<Dictionary<string, int>, string, ValueTask> item, Dictionary<string, int> activeBufferFile, string email);
+    ValueTask<Func<Dictionary<string, int>, string, ValueTask>> Dequeue(CancellationToken cancellationToken);
+    Dictionary<string, int> GetFile();
+    string GetEmail();
+  }
+
+  public class BackgroundTaskQueue : IBackgroundTaskQueue
+  {
+    private readonly Channel<Func<Dictionary<string, int>, string, ValueTask>> _queue;
+    public Dictionary<string, int> File { get; private set; }
+    public string Email { get; private set; }
+
+    public BackgroundTaskQueue()
+    {
+      var options = new BoundedChannelOptions(10)
+      {
+        FullMode = BoundedChannelFullMode.Wait
+      };
+
+      _queue = Channel.CreateBounded<Func<Dictionary<string, int>, string, ValueTask>>(options);
+    }
+
+    public async ValueTask Enqueue(Func<Dictionary<string, int>, string, ValueTask> item, Dictionary<string, int> activeBufferFile, string email)
+    {
+      if (item == null)
+      {
+        throw new ArgumentNullException(nameof(item));
+      }
+
+      File = activeBufferFile;
+      await _queue.Writer.WriteAsync(item);
+    }
+
+    public async ValueTask<Func<Dictionary<string, int>, string, ValueTask>> Dequeue(CancellationToken cancellationToken)
+    {
+      return await _queue.Reader.ReadAsync(cancellationToken);
+    }
+
+    public Dictionary<string, int> GetFile() => File;
+
+    public string GetEmail() => Email;
+  }
+}
---
+++ NewAgeUI/BackgroundServices/QueuedBackgroundService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NewAgeUI.BackgroundServices
+{
+  public class QueuedBackgroundService : BackgroundService
+  {
+    private readonly IBackgroundTaskQueue _queue;
+
+    public QueuedBackgroundService(IBackgroundTaskQueue queue)
+    {
+      _queue = queue;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+      while (!stoppingToken.IsCancellationRequested)
+      {
+        var item = await _queue.Dequeue(stoppingToken);
+
+        try
+        {
+          await item(_queue.GetFile(), _queue.GetEmail());
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine(ex.Message);
+        }
+      }
+    }
+  }
+}
