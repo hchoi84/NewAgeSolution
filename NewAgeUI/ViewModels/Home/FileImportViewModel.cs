8f175efdaf9fef89d02298d2fadf515c086aba1e
hchoi84 <hchoi84@gmail.com>
2021-09-01 16:38:32 -0700

    Buffer setter page error handling and email input

--- NewAgeUI/ViewModels/Home/FileImportViewModel.cs
+++ NewAgeUI/ViewModels/Home/FileImportViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace NewAgeUI.ViewModels
 {
   public class FileImportViewModel
   {
+    [Required(ErrorMessage = "Required")]
     public IFormFile CSVFile { get; set; }
+    
+    [Required(ErrorMessage = "Required")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
   }
 }
